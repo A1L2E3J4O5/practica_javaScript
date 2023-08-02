976287827482716820fe4dbac125e5c773b62865
jesus ramirez <135658843+A1L2E3J4O5@users.noreply.github.com>
2023-08-02 14:54:51 -0500

    practica 1 array pso 5

---
+++ array1/array.js
@@ -0,0 +1,17 @@
+let myArray = Array.of
+(
+    "sistem of a down" ,
+    ["Pizza"],
+    Array.of("mago de oz"),
+    "lasaña"
+)
+
+
+console.log(myArray[0],myArray[1],myArray[2],myArray[3],)
+
+
+
+//myArray.forEach(function(el){
+    //console.log( `<li> ${el}</li>` )
+//}
+//)
