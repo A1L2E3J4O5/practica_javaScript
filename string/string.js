eab694ff8f73135768e583eefc31d7131e607ba2
jesus ramirez <135658843+A1L2E3J4O5@users.noreply.github.com>
2023-08-02 11:26:13 -0500

    practica string realizada

--- string/string.js
+++ string/string.js
@@ -2,21 +2,25 @@ let miRefran
 
 miRefran = "camaron que se duerme se lo lleva la corriente"
 
-console.log(miRefran)
+    console.log(miRefran)
 
 let longitudRefran = miRefran.length
 
-console.log(longitudRefran)
+    console.log("la cantidad de letras de mi refran es: ",longitudRefran)
 
 let novenaLetra=miRefran[9]
 
-console.log(novenaLetra)
+    console.log("La novena letra de mi refran es: ",novenaLetra)
 
 let ultimaLetra = miRefran[45]
 
-console.log(ultimaLetra)
+    console.log("La ultima letra de mi refran es: ",ultimaLetra)
 
 const indicePalabra = miRefran.indexOf("que")
 
-console.log(indicePalabra)
+    console.log("El indice de la palabra que en mi refran esta en: ",indicePalabra)
 
+    console.log(miRefran.replace("que","xxx"));
+
+    console.log(miRefran.toLocaleUpperCase());
+    
