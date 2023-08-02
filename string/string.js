218b391369074c6c1949029363bd2867c78092f9
jesus ramirez <135658843+A1L2E3J4O5@users.noreply.github.com>
2023-08-02 10:44:41 -0500

    string hasta paso 6 en main

---
+++ string/string.js
@@ -0,0 +1,22 @@
+let miRefran
+
+miRefran = "camaron que se duerme se lo lleva la corriente"
+
+console.log(miRefran)
+
+let longitudRefran = miRefran.length
+
+console.log(longitudRefran)
+
+let novenaLetra=miRefran[9]
+
+console.log(novenaLetra)
+
+let ultimaLetra = miRefran[45]
+
+console.log(ultimaLetra)
+
+const indicePalabra = miRefran.indexOf("que")
+
+console.log(indicePalabra)
+
