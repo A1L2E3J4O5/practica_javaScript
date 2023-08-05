7b4fdb21cbb1898b04d19f3a7bc688e8a1195cca
jesus ramirez <135658843+A1L2E3J4O5@users.noreply.github.com>
2023-08-04 20:37:53 -0500

    practica condicionales1 realizada

---
+++ condicionales1/condicionales1.js
@@ -0,0 +1,13 @@
+let uno = prompt("elige un numero");
+let dos = prompt("elige otro numero");
+let uno1 = parseInt(uno);
+let dos1 = parseInt(dos);
+
+let suma = uno1 + dos1;
+
+alert(suma);
+
+
+
+
+//console.log("")
